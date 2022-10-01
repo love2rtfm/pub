e2a2cb898889c70e35ccd0eac777eaa48c0fe723
love2rtfm <102277127+love2rtfm@users.noreply.github.com>
2022-10-01 16:07:16 -0700

    Create arch-desktop-stuff.sh

---
+++ arch-desktop-stuff.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+useradd -G wheel -m e
+echo "e:changeme" | chpasswd
+
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+
+# network
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
+# sshd
+pacman -S --noconfirm openssh
+systemctl enable sshd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+systemctl start sshd
+
+# extra stuff
+pacman -S --noconfirm usbutils bash-completion podman traceroute base-devel
+
+# i3 desktop
+pacman -S --noconfirm i3
+
+# nx
+git clone https://aur.archlinux.org/nomachine.git
+cd nomachine
+makepkg -si
+sudo pacman -U --noconfirm nomachine*pkg*xz
+
+sudo systemctl enable nxserver.service
+sudo systemctl start nxserver.service
